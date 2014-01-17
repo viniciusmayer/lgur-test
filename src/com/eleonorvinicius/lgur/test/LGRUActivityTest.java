0760b98c2ea2c3cd515fcb758b1ae3be2f3046c6
Vinicius Mayer <viniciusmayer@gmail.com>
2014-01-17 18:26:06 -0200

    initial commit

---
+++ src/com/eleonorvinicius/lgur/test/LGRUActivityTest.java
@@ -0,0 +1,11 @@
+package com.eleonorvinicius.lgur.test;
+
+import junit.framework.TestCase;
+
+public class LGRUActivityTest extends TestCase {
+
+	public void testFake() throws Exception {
+		
+	}
+	
+}
